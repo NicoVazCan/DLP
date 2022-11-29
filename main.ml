3be881f59ea18c17aa139553c580f4352e7ac945
11martincf <11martincf@gmail.com>
2022-11-29 19:04:30 +0100

    Añadido multilinea

--- main.ml
+++ main.ml
@@ -6,15 +6,24 @@ open Lambda;;
 open Parser;;
 open Lexer;;
 
+let del = ';';;
+
 let top_level_loop () =
   print_endline "Evaluator of lambda expressions...";
   let rec loop ctx =
+    let print_term linea = (
+      let tm = s token (from_string (linea)) in
+            let tyTm = typeof ctx tm in
+            print_endline (string_of_term (eval tm) ^ " : " ^ string_of_ty tyTm)) in
     print_string ">> ";
     flush stdout;
     try
-      let tm = s token (from_string (read_line ())) in
-      let tyTm = typeof ctx tm in
-      print_endline (string_of_term (eval tm) ^ " : " ^ string_of_ty tyTm);
+      let rec aux s = 
+        let linea = read_line() in
+        if String.contains linea del
+        then print_term (s ^ (List.hd (String.split_on_char del linea)))
+        else aux (s ^ linea)
+      in aux "";
       loop ctx
     with
        Lexical_error ->
