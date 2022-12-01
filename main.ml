530e03eadedb6f71df64d3f2af3c8847d93b815d
NicoVazCan <nicolas.vazquez.cancela@udc.es>
2022-12-01 22:36:35 +0100

    Corregidos fallos en el merge

--- main.ml
+++ main.ml
@@ -10,36 +10,34 @@ let del = ';';;
 
 let top_level_loop () =
   print_endline "Evaluator of lambda expressions...";
-  let rec loop ctx =
-    let print_term linea = (
-      let tm = s token (from_string (linea)) in
-            let tyTm = typeof ctx tm in
-            print_endline (string_of_term (eval tm) ^ " : " ^ string_of_ty tyTm)) in
+  let rec loop ctxs =
     print_string ">> ";
     flush stdout;
     try
-      let rec aux s = 
-        let linea = read_line() in
-        if String.contains linea del
-        then print_term (s ^ (List.hd (String.split_on_char del linea)))
-        else aux (s ^ linea)
+      let rec aux cmd = 
+        let line = read_line() in
+        let cmd' = cmd ^ (List.hd (String.split_on_char del line)) in
+        if String.contains line del then
+          let tm = s token (from_string cmd') in
+          loop (execute ctxs tm)
+        else aux cmd'
       in aux "";
-      loop ctx
+      
     with
        Lexical_error ->
          print_endline "lexical error";
-         loop ctx
+         loop ctxs
      | Parse_error ->
          print_endline "syntax error";
-         loop ctx
+         loop ctxs
      | Type_error e ->
          print_endline ("type error: " ^ e);
-         loop ctx
+         loop ctxs
      | End_of_file ->
          print_endline "...bye!!!"
   in
-    loop emptyctx
-  ;;
+    loop (emptyvctx, emptytctx)
+;;
 
 top_level_loop ()
 ;;
